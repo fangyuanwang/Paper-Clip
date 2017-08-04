cf0dcba7ac56ae5d2bed27068d0b327a891e027e
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-08-03 20:26:43 -0400

    added function for fab to my-note-list page

--- PaperClip/src/app/notes/my-note-list/my-note-list.component.ts
+++ PaperClip/src/app/notes/my-note-list/my-note-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { NoteService, NoteRoute } from "app/services/note.service";
+import { MdDialogConfig, MdDialog } from "@angular/material";
+import { NoteDialogComponent } from "app/notes/note-dialog/note-dialog.component";
 
 @Component({
   selector: 'app-my-note-list',
@@ -10,10 +12,16 @@ import { NoteService, NoteRoute } from "app/services/note.service";
 export class MyNoteListComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
-    public noteService: NoteService) { }
+    public noteService: NoteService,
+    private dialog: MdDialog) { }
 
   ngOnInit() {
     this.noteService.showMyNotesRoute(NoteRoute.mynotes);
   }
 
+  showNoteDialog(): void {
+    const dialogConfig = new MdDialogConfig();
+    dialogConfig.data = {};
+    this.dialog.open(NoteDialogComponent, dialogConfig);
+  }
 }
